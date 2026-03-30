7dd8c263829ab404d18a2fd308926d8879bb38ab
EmrhnEmiroglu <emirhanemiroglu33@gmail.com>
2026-03-30 12:19:04 +0300

    feat(cpt): add footballer post type and admin meta

--- guess-the-footballer.php
+++ guess-the-footballer.php
@@ -11,3 +11,16 @@
  */
 
 defined('ABSPATH') || exit;
+
+define('GTF_VERSION', '0.1.0');
+define('GTF_PLUGIN_DIR', plugin_dir_path(__FILE__));
+define('GTF_PLUGIN_URL', plugin_dir_url(__FILE__));
+
+require_once GTF_PLUGIN_DIR . 'includes/class-footballer-cpt.php';
+
+function gtf_init_plugin() {
+    if (class_exists('GTF_Footballer_CPT')) {
+        GTF_Footballer_CPT::init();
+    }
+}
+add_action('plugins_loaded', 'gtf_init_plugin');
